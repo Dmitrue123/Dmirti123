67b9f53029b14c8f64f9e522cc8ee8ce050a4883
Dmitrue123 <124274588+Dmitrue123@users.noreply.github.com>
2023-02-09 19:02:45 +0400

    Add files via upload

---
+++ DD.123.pin pong.py
@@ -0,0 +1,121 @@
+from pygame import *
+
+
+back = (200, 255, 255)
+win_width = 600
+win_height = 500
+window = display.set_mode((win_width, win_height))
+
+
+
+class GameSprite(sprite.Sprite):
+
+#конструктор класса
+
+  def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+
+      #вызываем конструктор класса (Sprite):
+
+      sprite.Sprite.__init__(self)
+
+      #каждый спрайт должен хранить свойство image - изображение
+
+      self.image = transform.scale(image.load(player_image), (size_x, size_y))
+
+      self.speed = player_speed
+
+      #каждый спрайт должен хранить свойство rect - прямоугольник, в который он вписан
+
+      self.rect = self.image.get_rect()
+
+      self.rect.x = player_x
+
+      self.rect.y = player_y
+
+  def reset(self):
+    window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
+class Player(GameSprite):
+    def update_r(self):
+        keys = key.get_pressed()
+        if keys[K_w] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_s] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+            
+    def update_l(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys[K_DOWN] and self.rect.y < win_height - 80:
+            self.rect.y += self.speed
+
+
+
+
+
+
+clock = time.Clock()
+FPS = 60
+game = True
+finish = False
+
+ball = GameSprite('mach.png', 5, win_height - 200, 80, 100, 10)
+
+update_l = Player('mac.png', 5, win_height - 300, 80, 100, 10 )
+update_r = Player('mac.png', 500, win_height - 300, 80, 100, 10 )
+
+font.init()
+
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+speed_x = 3
+speed_y = 3
+
+while game:
+
+    ball.update()
+    update_l.update()
+    update_r.update()
+    
+
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    if finish != True:
+
+        window.fill(back)
+
+        ball.rect.x += speed_x        
+        ball.rect.y += speed_y  
+
+    if ball.rect.y > win_height - 50 or ball.rect.y < 0:
+        speed_y *= -1
+    
+    if sprite.collide_rect(update_l, ball) or sprite.collide_rect(update_r, ball):
+        speed_x *= -1
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose1, (200, 200))
+
+    if ball.rect.x < 0:
+        finish = True
+        window.blit(lose2, (200, 200))
+
+
+
+
+
+    ball.reset()
+    update_l.reset()
+    update_r.reset()
+
+display.update()
+clock.tick(FPS)
